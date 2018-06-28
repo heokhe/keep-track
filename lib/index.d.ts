c9d7d9364c089c64c9afe9bb14a68f0d02a74c25
Hosein Khansari <hrk.x11@gmail.com>
2018-06-28 18:56:39 +0430

    fix typings

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,5 @@
 declare type keyName = string | number
-declare type KT_handler = (newValue: any, oldValue: any, key: keyName, object: object) => ay
+declare type KT_handler = (newValue: any, oldValue: any, key: keyName, object: object) => any
 declare interface KT_Handlers {
 	willSet?: KT_handler
 	didSet?: KT_handler
